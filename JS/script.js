8294666247a3c299cb6dbd96b3f0ce35bfba655c
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2025-02-08 00:55:45 +0800

    get result

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,6 @@ let fahrentempera = document.getElementById('fahrenheit');
 
 function savetempera(e){
   
- 
-
 
 }
 
@@ -15,13 +13,28 @@ function savetempera(e){
 
 
 function convertCelciustoFahrenheit(){
-    let celsius = document.getElementById("temperature").value;
-    let fahrenheit = (celsius * 9/5) + 32;
-    document.getElementById('fahrenheit').value = fahrenheit;
-    console.log(fahrenheit);
+    try{    
+        if(document.getElementById("temperature").value==""){
+            alert("Please enter a temperature value");
+            return;
+        }else{
+            let celsius = document.getElementById("temperature").value;
+            let fahrenheit = (celsius * 9/5) + 32;
+            document.getElementById('fahrenheit').value = fahrenheit;
+            document.getElementById('calculationMethod').value= `(${celsius}*9/5)+32`;
+        }
+    }catch(error){
+        alert("An error occurred: " + error.message);
+    }
 
 }
 
-// fahrentempera.value=fahrenheit;
+
+function resetvalue(){
+    document.getElementById('temperature').value = "";
+    document.getElementById('fahrenheit').value = "";
+    document.getElementById('calculationMethod').value = "";
+}
+
 
 
