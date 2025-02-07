15a5338af3378bbba4080bab0d35d63c258587a2
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2025-02-08 00:37:16 +0800

    Celcius to Fahrenheit Works

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
-aaaa
+let tempera = document.getElementById('temperature');
+let temperaa;
+let fahrentempera = document.getElementById('fahrenheit');
+
+
+function savetempera(e){
+  
+ 
+
+
+}
+
+
+
+
+
+function convertCelciustoFahrenheit(){
+    let celsius = document.getElementById("temperature").value;
+    let fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById('fahrenheit').value = fahrenheit;
+    console.log(fahrenheit);
+
+}
+
+// fahrentempera.value=fahrenheit;
+
+
